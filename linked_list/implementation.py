0130694e044a8a47714eace85403f822f4472457
Santiago Basulto <santiago.basulto@gmail.com>
2016-07-14 17:26:18 -0300

    Initial refactor

---
+++ linked_list/implementation.py
@@ -0,0 +1,40 @@
+from .interface import AbstractLinkedList
+
+
+class LinkedList(AbstractLinkedList):
+    """
+    Implementation of an AbstractLinkedList inteface.
+    """
+
+    def __init__(self, elements=None):
+        pass
+
+    def __str__(self):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __iter__(self):
+        pass
+
+    def __getitem__(self, index):
+        pass
+
+    def __add__(self, other):
+        pass
+
+    def __iadd__(self, other):
+        pass
+
+    def __eq__(self, other):
+        pass
+
+    def append(self, elem):
+        pass
+
+    def count(self):
+        pass
+
+    def pop(self, index=None):
+        pass
--- linked_list/interface.py
+++ linked_list/interface.py
@@ -1,7 +1,7 @@
 class AbstractLinkedList(object):
     """
     Abstract class representing the LinkedList inteface you must respect.
-    
+
     You must not implement any of the method in this class, and this class
     must never be instantiated. It's just a "guide" of which methods
     the LinkedList class should respect.
@@ -36,60 +36,3 @@ class AbstractLinkedList(object):
 
     def pop(self, index=None):
         raise NotImplementedError()
-
-
-class Node(object):
-    """
-    Node class representing each of the linked nodes in the list.
-    """
-
-    def __init__(self, elem, next=None):
-        pass
-
-    def __str__(self):
-        pass
-
-    def __eq__(self, other):
-        pass
-
-    def __repr__(self):
-        pass
-
-
-class LinkedList(AbstractLinkedList):
-    """
-    Implementation of an AbstractLinkedList inteface.
-    """
-
-    def __init__(self, elements=None):
-        pass
-
-    def __str__(self):
-        pass
-
-    def __len__(self):
-        pass
-
-    def __iter__(self):
-        pass
-
-    def __getitem__(self, index):
-        pass
-
-    def __add__(self, other):
-        pass
-
-    def __iadd__(self, other):
-        pass
-
-    def __eq__(self, other):
-        pass
-
-    def append(self, elem):
-        pass
-
-    def count(self):
-        pass
-
-    def pop(self, index=None):
-        pass
