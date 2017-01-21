c65e606a47331ac93b1c38c6c737fd0c6b006f09
Vojtech Kotek <kotek.vojtech@gmail.com>
2017-01-21 19:06:41 +0100

    Replaced

--- linked_list/node.py
+++ linked_list/node.py
@@ -1,16 +1,28 @@
 class Node(object):
     """
     Node class representing each of the linked nodes in the list.
+    * Each node has a value and a reference to the next Node in the list.
     """
 
+    # Initialize node, next is class
     def __init__(self, elem, next=None):
+        self.elem = elem
+        self.next = next
         pass
 
+    # elem > next
     def __str__(self):
-        pass
+        if self.next:
+            return "Node({}) > Node({})".format(self.elem, self.next.elem)
+        else:
+            return "Node({}) > /".format(self.elem)
 
     def __eq__(self, other):
-        pass
-
+        return self.elem == other.elem and self.next == other.next
+    
+    # When enabled things get weird and tests don't pass.
+    # def __ne__(self, other):
+    #     return self.elem != other.elem and self.next != other.next
+        
     def __repr__(self):
-        pass
+        return str(self.elem)
